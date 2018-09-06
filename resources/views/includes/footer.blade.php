9754c660da75abbbfe5bb0e55d11571d17445113
David Magro <david.magro@ex-gr.com>
2018-09-06 17:57:43 +0200

    43. Directiva @include para un footer general

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,31 @@
+<footer class="footer">
+    <div class="container">
+        <nav class="pull-left">
+            <ul>
+                <li>
+                    <a href="">
+                        Novedades
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        Más Vendidos
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        Blog
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        About Us
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <div class="copyright pull-right">
+            &copy; 2016, made with <i class="fa fa-heart heart"></i> by Creative Tim
+        </div>
+    </div>
+</footer>
