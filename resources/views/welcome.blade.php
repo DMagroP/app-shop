bf60cea5924a9fe34f0dbdd56335d6a46740c404
David Magro <david.magro@ex-gr.com>
2018-09-07 13:00:30 +0200

    47. Accessor para la imagen destacada

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,7 +69,7 @@
                     @foreach($products as $product)
                     <div class="col-md-4">
                         <div class="team-player">
-                            <img src="{{ $product->images()->first() ? $product->images()->first()->image : asset('img/default.png')  }}" alt="Thumbnail Image" class="img-raised img-circle">
+                            <img src="{{ $product->featured_image_url }}" alt="Thumbnail Image" class="img-raised img-circle">
                             <h4 class="title">{{ $product->name }}<br />
                                 <small class="text-muted">{{ $product->category ? $product->category->name : 'General' }}</small>
                             </h4>
